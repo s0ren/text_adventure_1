7a5f42648cc79175ff963a887b8c232936bbeb76
Søren Magnusson <soren.magnusson@dinstu.dk>
2024-04-24 14:19:40 +0200

    loop, but messages not perfect

--- soren_game.py
+++ soren_game.py
@@ -3,8 +3,9 @@
 print("You wake up in a mysterious place.")
 
 cave_description = "You are in a dim lit room. You see a table, and on the wall a door"
-
+scene_description = cave_description
 print(cave_description)
+
 
 prompt = "You can now make action by typing verbs and items. "
 verbs = ["go to", "switch on", "enable" ] # more to come
@@ -13,26 +14,32 @@ items = ["table", "door", "lamp", "Lock", "key"] # maybee item shall have states
 positions = ["floor", "table", "wall"]
 current_pos = "floor"
 
-print("Verbs:", verbs)
-print("items:", items)
-print(prompt)
-command = input("> ")
+game_over = False
 
-# debug info
-print("command:", command)
+while not game_over:
+    print(scene_description)
+    print("Verbs:", verbs)
+    print("items:", items)
+    print(prompt)
+    command = input("> ")
 
-verb = command[0 : command.rfind(' ')]
-item = command[command.rfind(' ')+1 : -1]
+    # debug info
+    print("command:", command)
 
-print(verb)
-print(item)
+    verb = command[0 : command.rfind(' ')]
+    item = command[command.rfind(' ')+1 : ]
 
-if verb == "go to":
-    pass
-    if item == "table":
-        current_pos = "table"
-        prompt = "You are next to a table. There is a lamp on it. The Lamp as turned off." # but what if the lamp is on?
-elif verb == "switch on" or verb == "enable":
-    pass 
+    print(verb)
+    print(item)
 
-print(prompt)
+    if verb == "go to":
+        pass
+        if item == "table":
+            current_pos = "table"
+            scene_description = "You are next to a table. There is a lamp on it. The Lamp as turned off." # but what if the lamp is on?
+        elif item == "door":
+            scene_description = "In the wall there is a door. There is a huge lock on the door. The door won't open!"    
+    elif verb == "switch on" or verb == "enable":
+        pass 
+
+    # print(prompt)
